4da4197fa8bfb5d01449e0028a17045e7880548c
Carla Stickler <carlastickler@gmail.com>
2019-09-18 00:10:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,17 +1,17 @@
 # code your #position_taken? method here!
 
-def position_taken?(board, index)
-  if board[index] == " "
-    taken = false
-  elsif board[index] == ""
-    taken = false
-  elsif board[index] == NIL
-    taken = false
-  else 
-    taken = true
-  end
-  taken
-end
+# def position_taken?(board, index)
+#   if board[index] == " "
+#     taken = false
+#   elsif board[index] == ""
+#     taken = false
+#   elsif board[index] == NIL
+#     taken = false
+#   else 
+#     taken = true
+#   end
+#   taken
+# end
 
 def position_taken?(board, index)
   (board[index] = "" || board[index] == " " || board[index] == NIL) ? true : false 
